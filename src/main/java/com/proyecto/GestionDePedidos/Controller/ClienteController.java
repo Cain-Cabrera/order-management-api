69b5332fa4d5a28c9cd124ea2c0f6272af8dd6f6
Cain-Cabrera <caincito.cabrera7@gmail.com>
2025-12-19 23:07:33 -0300

    creamos el controlador para cliente y pedido, 4 metodos sencillos para probar funcionalidades.. el controlador esta enfocado en controlar el flujo http,por buenas practicas,respetando SRP, no maneja logica ni persistencia.

--- src/main/java/com/proyecto/GestionDePedidos/Controller/ClienteController.java
+++ src/main/java/com/proyecto/GestionDePedidos/Controller/ClienteController.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.proyecto.GestionDePedidos.Controller;
 
+import com.proyecto.GestionDePedidos.DTO.ClienteRequestDTO;
+import com.proyecto.GestionDePedidos.DTO.ClienteResponseDTO;
+import com.proyecto.GestionDePedidos.Service.ClienteServiceImple;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 /**
  *
  * @author Cain
  */
+@RestController
 public class ClienteController {
+    private final ClienteServiceImple clienteService;
+    
+    private static final Logger logger = LoggerFactory.getLogger(ClienteController.class);
+
+    public ClienteController(ClienteServiceImple clienteService) {
+        this.clienteService = clienteService;
+    }
+    
+    @PostMapping("/cliente")
+    public ResponseEntity<ClienteResponseDTO> createCliente(@Valid @RequestBody ClienteRequestDTO clienteRequest) {
+        logger.trace("Se ejecuta endpoint 'createCliente'..");
+        ClienteResponseDTO clienteResponse = clienteService.createCliente(clienteRequest);
+        return new ResponseEntity(clienteResponse, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<ClienteResponseDTO> findByid(@PathVariable Long id) {
+        logger.trace("Se ejecuta metodo findByid para buscar un cliente via id");
+        ClienteResponseDTO response = clienteService.findById(id);
+        return new ResponseEntity(response, HttpStatus.OK); 
+    }
+    
+    @PutMapping("/cliente/{id}")
+    public ResponseEntity<ClienteResponseDTO> updateCliente(@Valid @RequestBody ClienteRequestDTO clienteRequest,@PathVariable Long id) {
+        logger.trace("Se ejecuta updateCliente para modificar cliente existente..");
+        ClienteResponseDTO clienteResponse = clienteService.updateCliente(id, clienteRequest);
+        return new ResponseEntity(clienteResponse, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/cliente/{id}") 
+    public ResponseEntity<String> deleteCliente(@PathVariable Long id) {
+        logger.trace("Se ejecuta metodo deleteCliente..");
+        clienteService.deleteCliente(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }
     
 }
--- src/main/java/com/proyecto/GestionDePedidos/Controller/PedidoController.java
+++ src/main/java/com/proyecto/GestionDePedidos/Controller/PedidoController.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.proyecto.GestionDePedidos.Controller;
+
+import com.proyecto.GestionDePedidos.DTO.PedidoRequestDTO;
+import com.proyecto.GestionDePedidos.DTO.PedidoResponseDTO;
+import com.proyecto.GestionDePedidos.Service.PedidoService;
+import jakarta.validation.Valid;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Cain
  */
+@RestController
 public class PedidoController {
+    private final PedidoService pedidoService;
+    private static final Logger logger = LoggerFactory.getLogger(PedidoController.class);
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
     
+    @PostMapping("/pedido")
+    public ResponseEntity<PedidoResponseDTO> createPedido(@Valid @RequestBody PedidoRequestDTO pedidoRequest) {
+        logger.trace("Se ejecuta createPedido para dar de alta entidad pedido..");
+        PedidoResponseDTO response = pedidoService.createPedido(pedidoRequest);
+        return new ResponseEntity(response, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/pedidos")
+    public ResponseEntity<List<PedidoResponseDTO>> findAll() {
+        logger.trace("Se ejectua findAll para traer todos los pedidos existentes..");
+        List<PedidoResponseDTO> response = pedidoService.findAll();
+        return new ResponseEntity(response, HttpStatus.OK);
+    } 
+    
+    @GetMapping("/pedido/{id}")
+    public ResponseEntity<PedidoResponseDTO> findById(@PathVariable Long id) {
+        logger.info("Se ejecuta finByid para traer entidad via id");
+        PedidoResponseDTO response = pedidoService.findById(id);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+            
+            
+    @DeleteMapping("/pedido/{id}")
+    public ResponseEntity<String> deletePedido(@PathVariable Long id) {
+        logger.info("Se ejecuta metodo deletePedido para borrar pedido existente..");
+        pedidoService.deletePedido(id);
+        return new ResponseEntity(HttpStatus.OK);
+    }  
 }
